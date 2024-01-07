d5db0bf74016c7458be834315275248402e91b3a
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-01-07 18:55:41 -0500

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,24 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+function About(){
+  return(
+    <About id="about">
+      <h2> About </h2>
+    </About>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About/>
+    
+    
     </div>
   );
 }
